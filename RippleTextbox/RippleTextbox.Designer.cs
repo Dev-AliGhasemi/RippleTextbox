84b68b1cb04113a1b3ea7569399077ef43c414f1
Ali <computerfreak.2968@gmail.com>
2020-05-15 16:43:52 +0430

    write all code and work fine .

---
+++ RippleTextbox/RippleTextbox.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace RippleTextbox
+{
+    partial class RippleTextbox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblRip = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblRip
+            // 
+            this.lblRip.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblRip.Location = new System.Drawing.Point(140, 0);
+            this.lblRip.Name = "lblRip";
+            this.lblRip.Size = new System.Drawing.Size(0, 2);
+            this.lblRip.TabIndex = 0;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 10;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // RippleTextbox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblRip);
+            this.Name = "RippleTextbox";
+            this.Size = new System.Drawing.Size(281, 2);
+            this.Load += new System.EventHandler(this.RippleTextbox_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRip;
+        private System.Windows.Forms.Timer timer;
+    }
+}
---
+++ RippleTextbox/RippleTextbox.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RippleTextbox
+{
+    public partial class RippleTextbox: UserControl
+    {
+        private static Color NormalColor, ErrorColor;
+        private const byte GAM = 15;
+        public RippleTextbox()
+        {
+            InitializeComponent();
+        }
+        public Color SetErrorColor
+        {
+            get { return ErrorColor; }
+            set { ErrorColor = value; }
+        }
+        public Color SetNormalColor
+        {
+            get { return NormalColor; }
+            set { NormalColor = value; }
+        }
+
+        private void RippleTextbox_Load(object sender, EventArgs e)
+        {
+            ComeToCenter();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (lblRip.Width <= this.Width)
+            {
+                lblRip.Width += GAM;
+                lblRip.Left = (this.Width - lblRip.Width) / 2;
+            }
+            else
+                timer.Stop();            
+        }
+
+        public void ShowError()
+        {
+            ComeToCenter();
+            lblRip.BackColor = ErrorColor;
+            timer.Start();
+        }
+        public void ShowNormal()
+        {
+            ComeToCenter();
+            lblRip.BackColor = NormalColor;
+            timer.Start();
+        }
+
+        private void ComeToCenter()
+        {
+            lblRip.Width = 0;
+            lblRip.Left = this.Width / 2;
+            lblRip.Height = this.Height;
+        }
+    }
+}
